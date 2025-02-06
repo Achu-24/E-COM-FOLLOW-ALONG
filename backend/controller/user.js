40999391fd45efdbc6fe28c8da866fcd4620ba9a
Hamshaverthini K <hamshaverthinik@gmail.com>
2025-02-06 10:34:13 +0530

    updated

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -6,6 +6,9 @@ const router = express.Router();
 const { upload } = require("../multer");
 const ErrorHandler = require("../utility/ErrorHandler");
 const catchAsyncErrors = require("../middleware/catchAsyncError");
+const jwt =require("jsonwebtoken");
+const sendmail= require("../utils/sendmail");
+const sendtoken = require("../utils/jwtToken");
 const bcrypt = require("bcryptjs");
 require("dotenv").config();
 
