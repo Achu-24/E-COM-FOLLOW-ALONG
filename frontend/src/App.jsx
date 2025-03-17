c114a217203f0a8c1efa5b06a96addb37f40b720
Hamshaverthini K <hamshaverthinik@gmail.com>
2025-03-17 10:32:47 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,33 @@
 /* eslint-disable no-unused-vars */
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css'
+import React from 'react'
+import {Login,Signup,Home, MyProducts,Cart, CreateAddress} from "./routes/Routes"
+import CreateProduct from './pages/CreateProduct';
+import ProductDetails from './pages/productDetails';
+import selectaddress from './pages/selectaddress';
+import orderconfirmation from './pages/orderconfirmation';
 
-import './App.css';
-import React from 'react';
-import {Login,Signup,Home, CreateProduct, MyProducts, Cart, ProductDetails} from "../Routes/Routes"
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import CreateAddress from './pages/createAddress';
-function App() {
-  return (
+
+function App(){
+  return(
     <BrowserRouter>
     <Routes>
-    <Route path='/' element={<Home/>}/>
-    <Route path='/login' element={<Login/>}/>
-    <Route path='/create-user' element={<Signup/>}/>
-    <Route path='/create-product' element={<CreateProduct/>}/>  
-    {/* For edit product by id */}
-    <Route path="/create-product/:id" element={<CreateProduct />} />
-    <Route path="/my-products" element={<MyProducts/>} />
-    <Route path="/cart" element={<Cart/>} />
-    <Route path="/product/:id" element={<ProductDetails />} />
-    <Route path="/createAddress" element={<CreateAddress />} />
+      <Route path='/' element={<Home/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/signup' element={<Signup/>}/>
+      <Route path='/Create-Product' element={<CreateProduct/>}/>
+      <Route path='/my-products' element={<MyProducts/>}/>
+      <Route path='/productDetails' element={<ProductDetails/>}/>
+      <Route path='/cart' element={<Cart/>}/>
+      <Route path='/createAddress' element={<CreateAddress/>}/>
+      <Route path='/SelectAddress' element={<SelectAddress/>}/>
+      <Route path='/OrderConfirmation' element={<OrderConfirmation/>}/>
+  
+      
     </Routes>
     </BrowserRouter>
-  );
+  )
+  
 }
-
 export default App;
